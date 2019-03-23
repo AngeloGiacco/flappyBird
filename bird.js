014b36051693cb0c5875ee6b986660ad8d95a881
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-23 23:00:14 +0000

    create score var

--- bird.js
+++ bird.js
@@ -1,13 +1,14 @@
 function Bird() {
   this.y = height/2;
   this.x = 64;
+  this.score = 0;
 
   this.gravity = 0.6;
   this.lift = -13;
   this.velocity = 0;
 
   this.show = function() {
-    fill(255);
+    fill(0,255,255);
     ellipse(this.x, this.y,32,32);
   }
 
