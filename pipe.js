53581855592222522ca39c2776b19c80eb934001
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-23 22:45:46 +0000

    create highlight when bird hits

--- pipe.js
+++ pipe.js
@@ -2,11 +2,27 @@ function Pipe() {
   this.top = random(height/2);
   this.bottom = random(height/2);
   this.x = width;
-  this.w = 20;
-  this.speed = 5
+  this.w = 30;
+  this.speed = 5;
+
+  this.highlight = false
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+  }
 
   this.show = function() {
     fill(255);
+    if (this.highlight) {
+      fill(255,0,0);
+    }
     rect(this.x, 0, this.w, this.top);
     rect(this.x,height-this.bottom, this.w,this.bottom);
   }
