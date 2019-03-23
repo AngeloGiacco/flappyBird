751e6c1e4bc76809ce707e29dd0b2dae7ced8115
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-23 18:11:46 +0000

    create gravity and lift

--- bird.js
+++ bird.js
@@ -2,12 +2,17 @@ function Bird() {
   this.y = height/2;
   this.x = 64;
 
-  this.gravity = 1;
+  this.gravity = 0.6;
+  this.lift = -13;
   this.velocity = 0;
 
   this.show = function() {
     fill(255);
     ellipse(this.x, this.y,32,32);
+  }
+
+  this.up = function() {
+    this.velocity += this.lift;
   }
 
   this.update = function() {
@@ -16,7 +21,7 @@ function Bird() {
     if (this.y > height){
       this.y = height;
       this.velocity = 0;
-    } elif (this.y < 0) {
+    } else if (this.y < 0) {
       this.y = 0;
       this.velocity = 0;
     }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 var bird;
-
 function setup() {
   createCanvas(400,600);
   bird = new Bird();
 }
-
 function draw() {
   background(0);
   bird.show()
+  bird.update();
+}
+
+function keyPressed() {
+  if (key == " ") {
+    bird.up();
+  }
 }
