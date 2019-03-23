22f3a14365b0f2641e962b49b270d620d7fe9e2c
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-23 22:29:17 +0000

    show pipes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 var bird;
+var pipes = []
 function setup() {
   createCanvas(window.innerWidth,window.innerHeight);
   bird = new Bird();
+  pipes.push(new Pipe())
 }
 function draw() {
   background(0);
   bird.show()
   bird.update();
+  for (var i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+    pipes[i].update();
+  }
 }
 
 function keyPressed() {
