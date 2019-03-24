11767af515e93b09f02172f43c69349fdcd3ec93
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-24 10:45:27 +0000

    improve pipe sizes

--- pipe.js
+++ pipe.js
@@ -1,6 +1,6 @@
 function Pipe() {
-  this.top = random(height/2);
-  this.bottom = random(height/2);
+  this.top = random(height-32);
+  this.bottom = height - this.top - random(200,300);
   this.x = width;
   this.w = 30;
   this.speed = 5;
@@ -9,9 +9,9 @@ function Pipe() {
   this.highlight = false
 
   this.hits = function(bird) {
-    if (bird.y < this.top || bird.y > height - this.bottom) {
-      if (bird.x > this.x && bird.x < this.x + this.w) {
-        bird.score = 0;
+    if (bird.x > this.x && bird.x < this.x + this.w) {
+      if  (bird.y < this.top || bird.y > height - this.bottom){
+        bird.end = true;
         this.highlight = true;
         return true;
       } else if (!this.passed) {
@@ -27,6 +27,8 @@ function Pipe() {
     fill(255);
     if (this.highlight) {
       fill(255,0,0);
+    } else if (this.passed) {
+      fill(0,255,0)
     }
     rect(this.x, 0, this.w, this.top);
     rect(this.x,height-this.bottom, this.w,this.bottom);
